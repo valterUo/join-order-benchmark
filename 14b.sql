a9c7f7c070093cbb6c1826e41a36fc443b592a88
Valter Uotila <valter.uotila@gmail.com>
2022-06-20 17:55:46 +0300

    Update 14b

--- 14b.sql
+++ 14b.sql
@@ -1,11 +1,9 @@
-SELECT MIN(mi_idx.info) AS rating,
-       MIN(t.title) AS western_dark_production
+SELECT MIN(t.title) AS western_dark_production
 FROM info_type AS it1,
      info_type AS it2,
      keyword AS k,
      kind_type AS kt,
      movie_info AS mi,
-     movie_info_idx AS mi_idx,
      movie_keyword AS mk,
      title AS t
 WHERE it1.info = 'countries'
@@ -23,7 +21,6 @@ WHERE it1.info = 'countries'
                   'German',
                   'USA',
                   'American')
-  AND mi_idx.info > '6.0'
   AND t.production_year > 2010
   AND (t.title LIKE '%murder%'
        OR t.title LIKE '%Murder%'
@@ -31,11 +28,6 @@ WHERE it1.info = 'countries'
   AND kt.id = t.kind_id
   AND t.id = mi.movie_id
   AND t.id = mk.movie_id
-  AND t.id = mi_idx.movie_id
   AND mk.movie_id = mi.movie_id
-  AND mk.movie_id = mi_idx.movie_id
-  AND mi.movie_id = mi_idx.movie_id
   AND k.id = mk.keyword_id
-  AND it1.id = mi.info_type_id
-  AND it2.id = mi_idx.info_type_id;
-
+  AND it1.id = mi.info_type_id;
