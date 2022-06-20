cdb0788b64e7e899ad204e66c8177879aa5acb53
Valter Uotila <valter.uotila@gmail.com>
2022-06-20 17:58:58 +0300

    Update 13a

--- 13a.sql
+++ 13a.sql
@@ -1,5 +1,4 @@
 SELECT MIN(mi.info) AS release_date,
-       MIN(miidx.info) AS rating,
        MIN(t.title) AS german_movie
 FROM company_name AS cn,
      company_type AS ct,
@@ -8,7 +7,6 @@ FROM company_name AS cn,
      kind_type AS kt,
      movie_companies AS mc,
      movie_info AS mi,
-     movie_info_idx AS miidx,
      title AS t
 WHERE cn.country_code ='[de]'
   AND ct.kind ='production companies'
@@ -21,9 +19,4 @@ WHERE cn.country_code ='[de]'
   AND mc.movie_id = t.id
   AND cn.id = mc.company_id
   AND ct.id = mc.company_type_id
-  AND miidx.movie_id = t.id
-  AND it.id = miidx.info_type_id
-  AND mi.movie_id = miidx.movie_id
-  AND mi.movie_id = mc.movie_id
-  AND miidx.movie_id = mc.movie_id;
-
+  AND mi.movie_id = mc.movie_id;
