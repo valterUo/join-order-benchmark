b1c3030febd6ed57add61b965cd96e2d4353e9fb
Valter Uotila <valter.uotila@gmail.com>
2022-06-20 16:03:36 +0300

    Update 1a

--- 1a.sql
+++ 1a.sql
@@ -4,7 +4,6 @@ SELECT MIN(mc.note) AS production_note,
 FROM company_type AS ct,
      info_type AS it,
      movie_companies AS mc,
-     movie_info_idx AS mi_idx,
      title AS t
 WHERE ct.kind = 'production companies'
   AND it.info = 'top 250 rank'
@@ -12,8 +11,4 @@ WHERE ct.kind = 'production companies'
   AND (mc.note LIKE '%(co-production)%'
        OR mc.note LIKE '%(presents)%')
   AND ct.id = mc.company_type_id
-  AND t.id = mc.movie_id
-  AND t.id = mi_idx.movie_id
-  AND mc.movie_id = mi_idx.movie_id
-  AND it.id = mi_idx.info_type_id;
-
+  AND t.id = mc.movie_id;
