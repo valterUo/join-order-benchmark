fa7c0dc72a3cb57fc141c828a217bd9ec11d3257
Valter Uotila <valter.uotila@gmail.com>
2022-06-20 17:33:45 +0300

    Update 31a

--- 31a.sql
+++ 31a.sql
@@ -1,5 +1,4 @@
 SELECT MIN(mi.info) AS movie_budget,
-       MIN(mi_idx.info) AS movie_votes,
        MIN(n.name) AS writer,
        MIN(t.title) AS violent_liongate_movie
 FROM cast_info AS ci,
@@ -9,7 +8,6 @@ FROM cast_info AS ci,
      keyword AS k,
      movie_companies AS mc,
      movie_info AS mi,
-     movie_info_idx AS mi_idx,
      movie_keyword AS mk,
      name AS n,
      title AS t
@@ -32,23 +30,16 @@ WHERE ci.note IN ('(writer)',
                   'Thriller')
   AND n.gender = 'm'
   AND t.id = mi.movie_id
-  AND t.id = mi_idx.movie_id
   AND t.id = ci.movie_id
   AND t.id = mk.movie_id
   AND t.id = mc.movie_id
   AND ci.movie_id = mi.movie_id
-  AND ci.movie_id = mi_idx.movie_id
   AND ci.movie_id = mk.movie_id
   AND ci.movie_id = mc.movie_id
-  AND mi.movie_id = mi_idx.movie_id
   AND mi.movie_id = mk.movie_id
   AND mi.movie_id = mc.movie_id
-  AND mi_idx.movie_id = mk.movie_id
-  AND mi_idx.movie_id = mc.movie_id
   AND mk.movie_id = mc.movie_id
   AND n.id = ci.person_id
   AND it1.id = mi.info_type_id
-  AND it2.id = mi_idx.info_type_id
   AND k.id = mk.keyword_id
   AND cn.id = mc.company_id;
-
