db9b15b0b6cb36d45098490b93358be7334ec638
Valter Uotila <valter.uotila@gmail.com>
2022-06-20 17:50:56 +0300

    Update 25b

--- 25b.sql
+++ 25b.sql
@@ -1,5 +1,4 @@
 SELECT MIN(mi.info) AS movie_budget,
-       MIN(mi_idx.info) AS movie_votes,
        MIN(n.name) AS male_writer,
        MIN(t.title) AS violent_movie_title
 FROM cast_info AS ci,
@@ -7,7 +6,6 @@ FROM cast_info AS ci,
      info_type AS it2,
      keyword AS k,
      movie_info AS mi,
-     movie_info_idx AS mi_idx,
      movie_keyword AS mk,
      name AS n,
      title AS t
@@ -28,17 +26,11 @@ WHERE ci.note IN ('(writer)',
   AND t.production_year > 2010
   AND t.title LIKE 'Vampire%'
   AND t.id = mi.movie_id
-  AND t.id = mi_idx.movie_id
   AND t.id = ci.movie_id
   AND t.id = mk.movie_id
   AND ci.movie_id = mi.movie_id
-  AND ci.movie_id = mi_idx.movie_id
   AND ci.movie_id = mk.movie_id
-  AND mi.movie_id = mi_idx.movie_id
   AND mi.movie_id = mk.movie_id
-  AND mi_idx.movie_id = mk.movie_id
   AND n.id = ci.person_id
   AND it1.id = mi.info_type_id
-  AND it2.id = mi_idx.info_type_id
   AND k.id = mk.keyword_id;
-
