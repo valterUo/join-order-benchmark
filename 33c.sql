50095180c4eee2c80a0061aa9496f9f0cfbc6cfc
Valter Uotila <valter.uotila@gmail.com>
2022-06-20 16:54:05 +0300

    Update 33c

--- 33c.sql
+++ 33c.sql
@@ -1,7 +1,5 @@
 SELECT MIN(cn1.name) AS first_company,
        MIN(cn2.name) AS second_company,
-       MIN(mi_idx1.info) AS first_rating,
-       MIN(mi_idx2.info) AS second_rating,
        MIN(t1.title) AS first_movie,
        MIN(t2.title) AS second_movie
 FROM company_name AS cn1,
@@ -13,8 +11,6 @@ FROM company_name AS cn1,
      link_type AS lt,
      movie_companies AS mc1,
      movie_companies AS mc2,
-     movie_info_idx AS mi_idx1,
-     movie_info_idx AS mi_idx2,
      movie_link AS ml,
      title AS t1,
      title AS t2
@@ -28,25 +24,15 @@ WHERE cn1.country_code != '[us]'
   AND lt.link IN ('sequel',
                   'follows',
                   'followed by')
-  AND mi_idx2.info < '3.5'
   AND t2.production_year BETWEEN 2000 AND 2010
   AND lt.id = ml.link_type_id
   AND t1.id = ml.movie_id
   AND t2.id = ml.linked_movie_id
-  AND it1.id = mi_idx1.info_type_id
-  AND t1.id = mi_idx1.movie_id
   AND kt1.id = t1.kind_id
   AND cn1.id = mc1.company_id
   AND t1.id = mc1.movie_id
-  AND ml.movie_id = mi_idx1.movie_id
   AND ml.movie_id = mc1.movie_id
-  AND mi_idx1.movie_id = mc1.movie_id
-  AND it2.id = mi_idx2.info_type_id
-  AND t2.id = mi_idx2.movie_id
   AND kt2.id = t2.kind_id
   AND cn2.id = mc2.company_id
   AND t2.id = mc2.movie_id
-  AND ml.linked_movie_id = mi_idx2.movie_id
-  AND ml.linked_movie_id = mc2.movie_id
-  AND mi_idx2.movie_id = mc2.movie_id;
-
+  AND ml.linked_movie_id = mc2.movie_id;
