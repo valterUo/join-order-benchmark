9c958c10c6c9b468546670eeb3eca18e7049b868
Valter Uotila <valter.uotila@gmail.com>
2022-06-20 17:44:09 +0300

    Update 26a

--- 26a.sql
+++ 26a.sql
@@ -1,5 +1,4 @@
 SELECT MIN(chn.name) AS character_name,
-       MIN(mi_idx.info) AS rating,
        MIN(n.name) AS playing_actor,
        MIN(t.title) AS complete_hero_movie
 FROM complete_cast AS cc,
@@ -10,7 +9,6 @@ FROM complete_cast AS cc,
      info_type AS it2,
      keyword AS k,
      kind_type AS kt,
-     movie_info_idx AS mi_idx,
      movie_keyword AS mk,
      name AS n,
      title AS t
@@ -31,23 +29,16 @@ WHERE cct1.kind = 'cast'
                     'claw',
                     'laser')
   AND kt.kind = 'movie'
-  AND mi_idx.info > '7.0'
   AND t.production_year > 2000
   AND kt.id = t.kind_id
   AND t.id = mk.movie_id
   AND t.id = ci.movie_id
   AND t.id = cc.movie_id
-  AND t.id = mi_idx.movie_id
   AND mk.movie_id = ci.movie_id
   AND mk.movie_id = cc.movie_id
-  AND mk.movie_id = mi_idx.movie_id
   AND ci.movie_id = cc.movie_id
-  AND ci.movie_id = mi_idx.movie_id
-  AND cc.movie_id = mi_idx.movie_id
   AND chn.id = ci.person_role_id
   AND n.id = ci.person_id
   AND k.id = mk.keyword_id
   AND cct1.id = cc.subject_id
-  AND cct2.id = cc.status_id
-  AND it2.id = mi_idx.info_type_id;
-
+  AND cct2.id = cc.status_id;
